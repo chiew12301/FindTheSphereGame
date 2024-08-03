20b9034fe57263f1737f10a39ea6a6d932f216a2
KurumiChiew <64896661+chiew12301@users.noreply.github.com>
2024-08-03 21:44:02 +0800

    BaseFPS

---
+++ Source/FindTheSphere/FindTheSphere.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FindTheSphere.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FindTheSphere, "FindTheSphere" );
+ 
---
+++ Source/FindTheSphere/FindTheSphereGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FindTheSphereGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFindTheSphereGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFindTheSphereGameMode();
+};
+
+
+
